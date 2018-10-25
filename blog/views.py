44ed5c4a7b48abba842ba2c1fe107ea0f7a1c6a9
fiffeek <fm394471@students.mimuw.edu.pl>
2018-10-25 15:19:37 +0200

    Fast bug fix with AnonymousUser (that account could make a follow)

--- blog/views.py
+++ blog/views.py
@@ -47,7 +47,7 @@ class PostListView(LoginRequiredMixin, ListView):
         return Post.objects.filter(author__in=follows).order_by('-date_posted')
 
 
-class UserPostListView(ListView):
+class UserPostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
